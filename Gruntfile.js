dec85122b9a2bdd15464862b008db39b4b20ffba
David Pfahler <david@excellenteasy.com>
2013-05-09 00:22:26 +0200

    more meaningful test

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'image_resize:resize', 'image_resize:no_overwrite', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'image_resize:resize', 'nodeunit', 'image_resize:no_overwrite', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- test/image_resize_test.js
+++ test/image_resize_test.js
@@ -47,26 +47,5 @@ exports.image_resize = {
     test.equal(actual, expected, 'TeslaTurbine.png should be the same file');
 
     test.done();
-  },
-  no_overwrite: function(test) {
-    test.expect(4);
-
-    var actual = grunt.file.read('tmp/gnu.jpg');
-    var expected = grunt.file.read('test/expected/gnu.jpg');
-    test.equal(actual, expected, 'gnu.jpg should be the same file');
-
-    actual = grunt.file.read('tmp/wikipedia.png');
-    expected = grunt.file.read('test/expected/wikipedia.png');
-    test.equal(actual, expected, 'wikipedia.png should be the same file');
-
-    actual = grunt.file.read('tmp/Rhododendron.jpg');
-    expected = grunt.file.read('test/expected/Rhododendron.jpg');
-    test.equal(actual, expected, 'Rhododendron.jpg should be the same file');
-
-    actual = grunt.file.read('tmp/TeslaTurbine.png');
-    expected = grunt.file.read('test/expected/TeslaTurbine.png');
-    test.equal(actual, expected, 'TeslaTurbine.png should be the same file');
-
-    test.done();
   }
 };
