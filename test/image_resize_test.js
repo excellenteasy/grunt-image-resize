6afc1829a7025d4533eedb4f0f6e4fe9072b7923
David Pfahler <david@excellenteasy.com>
2013-05-09 12:20:43 +0200

    test only width of images, not exact bit equality

--- test/image_resize_test.js
+++ test/image_resize_test.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var grunt = require('grunt');
+var grunt = require('grunt'),
+    im    = require('node-imagemagick'),
+    async = require('async');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -30,22 +32,23 @@ exports.image_resize = {
   resize: function(test) {
     test.expect(4);
 
-    var actual = grunt.file.read('tmp/gnu.jpg');
-    var expected = grunt.file.read('test/expected/gnu.jpg');
-    test.equal(actual, expected, 'gnu.jpg should be the same file');
+    var createTest = function(filename) {
+      return function(callback) {
+        im.identify('tmp/'+filename, function(err, features) {
+          im.identify('test/expected/'+filename, function(err, expected) {
+            test.equal(features.width, expected.width);
+            callback();
+          });
+        });
+      };
+    };
 
-    actual = grunt.file.read('tmp/wikipedia.png');
-    expected = grunt.file.read('test/expected/wikipedia.png');
-    test.equal(actual, expected, 'wikipedia.png should be the same file');
+    async.series([
+      createTest('gnu.jpg'),
+      createTest('Rhododendron.jpg'),
+      createTest('wikipedia.png'),
+      createTest('TeslaTurbine.png'),
+    ], test.done);
 
-    actual = grunt.file.read('tmp/Rhododendron.jpg');
-    expected = grunt.file.read('test/expected/Rhododendron.jpg');
-    test.equal(actual, expected, 'Rhododendron.jpg should be the same file');
-
-    actual = grunt.file.read('tmp/TeslaTurbine.png');
-    expected = grunt.file.read('test/expected/TeslaTurbine.png');
-    test.equal(actual, expected, 'TeslaTurbine.png should be the same file');
-
-    test.done();
   }
 };
